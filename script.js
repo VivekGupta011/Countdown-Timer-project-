49ce44a98d27d5f25cddfe850c78e6b40fd65878
Vivek_Gupta <vivekgupta86988@gmail.com>
2022-07-25 09:45:06 +0530

    countdown project

--- script.js
+++ script.js
@@ -9,20 +9,20 @@ function countdown() {
   const newyearsDate = new Date(newyears);
   const currentDate = new Date();
 
-  const totalseconds = (newyearsDate - currentDate) / 1000;
+  const totalseconds = (currentDate-newyearsDate) / 1000;
   const days= Math.floor(totalseconds / 3600 / 24);
   const hours = Math.floor(totalseconds / 3600) % 24;
   const mins = Math.floor(totalseconds / 60) % 60;
   const seconds = Math.floor(totalseconds) % 60;
 
-  days_p1.innerHTML = days;
+  days_p1.innerHTML = formatTime(days);
   hours_p1.innerHTML = formatTime(hours);
   minute_p1.innerHTML = formatTime(mins);
   second_p1.innerHTML = formatTime(seconds);
 }
 
 function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
+  return time < 10 ? `${time}` : time;
 }
 
 // initial call
